af0a86b95a97932f0244d6fbc6613d13d8040673
Johnmarc22 <146808774+Johnmarc22@users.noreply.github.com>
2023-10-09 10:37:28 +0800

    Update act1.js

---
+++ act1.js
@@ -0,0 +1,9 @@
+let firstFavNum = 1113, secondFavNum = 625; 
+  
+ console.log(firstFavNum + secondFavNum); 
+ console.log(firstFavNum - secondFavNum); 
+ console.log(firstFavNum * secondFavNum); 
+ console.log(firstFavNum / secondFavNum); 
+ console.log(firstFavNum % secondFavNum); 
+ let power = Math.pow(firstFavNum); 
+ console.log(firstFavNum);
