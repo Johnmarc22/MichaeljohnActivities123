90e772cc2b692ae26f1fffc9c3a5f800d1b179ca
Johnmarc22 <146808774+Johnmarc22@users.noreply.github.com>
2023-10-14 09:17:47 +0800

    Add files via upload

--- act7.js
+++ act7.js
@@ -1,9 +1,9 @@
-let firstName = " Michael John"; 
+let firstName = "Michael John"; 
  let last_name = " Macayan "; 
  let fullName = firstName + ' ' + last_name; 
  console.log (fullName); 
  console.log("BSCS-2A"); 
- console.log("CS201-BMI-LAB7"); 
+ console.log("CS201-MJEM-LAB7"); 
   
  let password = 'p@ssw0rd'; 
  if ( password.length >= 8 &&  password.includes("p@ss")){ 
---
+++ act8.js
@@ -0,0 +1,17 @@
+const favSingers = ["Siakol", "Daughtry", "Post Malone "];   
+  console.log (favSingers);   
+  const favNumbers = ["25","6","11","13"];   
+  console.log (favNumbers);   
+  const mixedArr = ["string",["otherarray"],123,true];   
+  
+  console.log (favSingers[0]);   
+  console.log (favSingers[1]);   
+  console.log (favSingers[2]);   
+  console.log (favNumbers[0]);   
+  console.log (favNumbers[1]);   
+  console.log (favNumbers[2]);   
+  console.log (favNumbers[3]);   
+  console.log (mixedArr[0]);   
+  console.log (mixedArr[1]);   
+  console.log (mixedArr[2]);   
+  console.log (mixedArr[3]);
