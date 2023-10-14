6607a9dcc4cf355a2e0739868f63a279ecd69887
Johnmarc22 <146808774+Johnmarc22@users.noreply.github.com>
2023-10-14 09:14:53 +0800

    Add files via upload

---
+++ activities7,8/act7.js
@@ -0,0 +1,13 @@
+let firstName = "Michael John"; 
+ let last_name = " Macayan "; 
+ let fullName = firstName + ' ' + last_name; 
+ console.log (fullName); 
+ console.log("BSCS-2A"); 
+ console.log("CS201-MJEM-LAB7"); 
+  
+ let password = 'p@ssw0rd'; 
+ if ( password.length >= 8 &&  password.includes("p@ss")){ 
+         console.log("Valid password"); 
+ } else{ 
+     console.log("Invalid password"); 
+ }
